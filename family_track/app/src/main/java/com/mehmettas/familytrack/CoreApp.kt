753bbaa0c3052ce70d98d3f3e0e26af9e6188ffb
mehmettas <mtas372372@hotmail.com>
2019-08-25 22:04:16 +0300

    CoreApp configurations

--- family_track/app/src/main/java/com/mehmettas/familytrack/CoreApp.kt
+++ family_track/app/src/main/java/com/mehmettas/familytrack/CoreApp.kt
@@ -1,4 +1,25 @@
 package com.mehmettas.familytrack
 
-class CoreApp {
+import android.app.Application
+import android.content.Context
+import com.mehmettas.familytrack.di.appModule
+import org.koin.android.ext.android.startKoin
+
+class CoreApp: Application() {
+
+    companion object{
+        lateinit var context:Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+        configureDI()
+    }
+
+    private fun configureDI() {
+        startKoin(this, appModule)
+    }
+
+
 }
